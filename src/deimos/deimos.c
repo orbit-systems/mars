0aa17e5e49150a3ffeafb53b753377258546c44f
spsandwichman <73349893+spsandwichman@users.noreply.github.com>
2024-04-18 17:58:07 -0400

    nuke some of deimos from orbit

--- src/deimos/deimos.c
+++ src/deimos/deimos.c
@@ -2,17 +2,10 @@
 
 arena deimos_alloca;
 
-void generate_return(AST node, da(IR)* ir_stream);
-
 void deimos_init(AST base_node) {
 	deimos_alloca = arena_make(DEIMOS_ARENA_SIZE);
 
 	// (sandwichman): hijacking this to test
-	da(IR) ir_stream = {0};
-	da_init(&ir_stream, 1);
-
-	generate_return(base_node, &ir_stream);
-	print_ir(ir_stream);
 
 	// init_passes();
 	// run_passes(base_node);
--- src/deimos/pass.h
+++ src/deimos/pass.h
@@ -4,27 +4,22 @@
 #include "../phobos/ast.h"
 #include "passes/passes.h"
 #include "deimos_ir.h"
-#include "deimos_ssa.h"
 
 void run_passes(AST base_node);
 
 void init_passes();
 
-IR pass_legalise(AST base_node);
+//IR pass_legalise(AST base_node);
 
 typedef enum {
 	PASS_IR_2_IR,
-	PASS_SSA_2_SSA,
-	//PASS_DAG_2_SSA,
-	//PASS_SSA_2_SSA,
 	PASS_ID_COUNT,
 } pass_id;
 
 typedef struct {
 	union {
 		void   (*callback)();
-		da(IR) (*IR_2_IR)(da(IR));
-		SSA    (*SSA_2_SSA)(SSA);
+		// da(IR) (*IR_2_IR)(da(IR));
 		//FIXME: include SSA stuff
 	};
 	string pass_name;
