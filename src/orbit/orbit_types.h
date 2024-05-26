374b632d4949bbc86c4a4b36f9b2211d2327bc8f
kaylatheegg <kaylasilkc@gmail.com>
2024-05-26 06:16:48 +0100

    refactors orbit.h to make actually using it nicer

---
+++ src/orbit/orbit_types.h
@@ -0,0 +1,19 @@
+#pragma once
+#define ORBIT_TYPES_H
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+typedef int64_t  i64;
+typedef int32_t  i32;
+typedef int16_t  i16;
+typedef int8_t   i8;
+
+typedef double   f64;
+typedef float    f32;
+typedef _Float16 f16;
+
+typedef uint8_t bool;
+#define false ((bool)0)
+#define true ((bool)1)
